99bd391daffed2a20b378cd32a335c59f12249e7
Guilherme Ehrat Zils <guilhermezils@icloud.com>
2021-01-03 01:22:14 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,11 +5,17 @@ def find_even_values(src)
 
 
 
-array_of_arrays = [
-  [10, 11], 
-  [99, 50, 3, 4], 
-  [23, 41] 
-  ]
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    while element_index < src[row_index].count do
+      # inside this loop, src[row_index][element_index] will access the current element
+      p 
+      element_index += 1
+    end
+    row_index += 1
+  end
+end
 
 
 end

3e98f89deda9a77fb753ebdb85d2b1146f0d9e3d
Guilherme Ehrat Zils <guilhermezils@icloud.com>
2021-01-03 01:19:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,33 @@ def find_even_values(src)
 
 
 
-array_of_integers = [10, 11], [99, 50, 3, 4], [23, 41] ]
+array_of_arrays = [
+  [10, 11], 
+  [99, 50, 3, 4], 
+  [23, 41] 
+  ]
 
 
 end
+
+
+
+
+
+
+
+spice_rack = [
+  ["Posh", "Scary", "Sporty"],
+  ["Paprika", "Fajita Mix", "Coriander"],
+  ["Parsley", "Sage", "Rosemary"]
+]
+ 
+row_index = 0
+while row_index < spice_rack.count do
+  element_index = 0
+  while element_index < spice_rack[row_index].count do
+    p spice_rack[row_index][element_index]
+    element_index += 1
+  end
+  row_index += 1
+end #=> nil
